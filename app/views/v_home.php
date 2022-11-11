c9bef932b071f239a39b6184944b09f1e2cde6b7
GimBlznf <88468262+GimBlznf@users.noreply.github.com>
2022-11-11 15:46:14 +0530

    ui-modifications

--- app/views/v_home.php
+++ app/views/v_home.php
@@ -1,7 +1,50 @@
+<?php require APPROOT.'/views/inc/components/navbars/home_nav.php'; ?>
 <?php require APPROOT.'/views/inc/components/header.php'; ?>
-<?php require APPROOT.'/views/inc/components/navbars/home_nav.php'; ?> 
-   <div class="home"></div>
 
+<div class="header-space">
+
+</div>
+
+<div class="white-space">
+    <br>
+    <h2 id="first-title" >Discover popular destinations across Sri Lanka!</h2>
+</div>
+
+<div class="scroll-bar" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
+    <img id="logo" src="<?php echo URLROOT; ?>/img/211689_left_arrow_icon.png" alt="logo">
+        
+    <img id="logo" style="height: 100px;width:auto" src="<?php echo URLROOT; ?>/img/25.jpg" alt="logo">
+    &nbsp;&nbsp;
+    <img id="logo" style="height: 200px;width:auto" src="<?php echo URLROOT; ?>/img/new.jpg" alt="nine-arch">
+    &nbsp;&nbsp;
+    <img id="logo" style="height: 100px;width:auto" src="<?php echo URLROOT; ?>/img/sigiriya-459197_1920.jpg" alt="sigiriya">
+    &nbsp;&nbsp;
+    <img id="logo" src="<?php echo URLROOT; ?>/img/211607_right_arrow_icon.png" alt="sigiriya">
+</div>
+
+<div class="three-dots">
+    <img id="logo" style="height: 100px;width:auto" src="<?php echo URLROOT; ?>/img/2205199_dot_hide_menu_more_icon" alt="scroll">
+</div>
+
+<hr class="divider">
+
+<div class="menu">
+    <div class="hotel-div">
+
+    </div>
+
+    <div class="taxi-div">
+
+    </div>
+
+    <div class="guide-div">
+
+    </div>
+
+    <div class="sights-div">
+        
+    </div>
+</div>
     <!-- <?php
     
         if ($data['isLoggedIn']) {
@@ -18,78 +61,8 @@
         }
 
     ?> -->
-    <div class="home-major">
-
-    <div class="white-space">
-        <div class="typed-out">
-            <h1 id="title" >Discover popular destinations across Sri Lanka!</h1>
-        </div> 
-
-    </div>
-    <br>
-    <br>
-
-    <div class="horizontal-scroll" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
-
-        <img id="logo" src="<?php echo URLROOT; ?>/img/211689_left_arrow_icon.png" alt="logo">
-
-        <img id="logo" style="height: 200px;width:auto" src="<?php echo URLROOT; ?>/img/25.jpg" alt="logo">
-        &nbsp;&nbsp;
-        <img id="logo" style="height: 200px;width:auto" src="<?php echo URLROOT; ?>/img/new.jpg" alt="nine-arch">
-        &nbsp;&nbsp;
-        <img id="logo" style="height: 200px;width:auto" src="<?php echo URLROOT; ?>/img/sigiriya-459197_1920.jpg" alt="sigiriya">
-        &nbsp;&nbsp;
-        <img id="logo" src="<?php echo URLROOT; ?>/img/211607_right_arrow_icon.png" alt="sigiriya">
-    </div>
-    <br>
-    <hr class="divider">
-
-    <h1 id="title" >Select what you want to search!</h1>
-
-    <div class="user-btns" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
-        <button id="btn-1" class="btns">
-            <span class="button__icon">
-                <ion-icon name="bed-outline"></ion-icon>
-            </span>
-            <br>
-            <span class="button__text">Hotel</span>
-        </button>&nbsp;&nbsp;&nbsp;&nbsp;
-
-        <button id="#2" class="btns">
-            <span class="button__icon">
-                <ion-icon name="car-outline"></ion-icon>
-            </span>
-            <br>
-            <span class="button__text">Taxi</span>
-        </button>&nbsp;&nbsp;&nbsp;&nbsp;
-
-        <button id="#3" class="btns">
-            <span class="button__icon">
-                <ion-icon name="accessibility-outline"></ion-icon>
-            </span>
-            <br>
-            <span class="button__text">Guide</span>
-        </button>&nbsp;&nbsp;&nbsp;&nbsp;
-
-        <button id="#4" class="btns">
-            <span class="button__icon">
-                <ion-icon name="map-outline"></ion-icon>
-            </span>
-            <br>
-            <span class="button__text">Plan a Trip</span>
-        </button>
-    </div>
-    
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-</body>
-</html>
-
 
 
 
     
-<?php require APPROOT.'/views/inc/components/footer.php'; ?>
 
-    </div>
-    
---
+++ app/views/v_hotel-page.php
@@ -0,0 +1,3 @@
+<?php require APPROOT.'/views/inc/components/navbars/home_nav.php'; ?>
+<?php require APPROOT.'/views/inc/components/header.php'; ?>
+
