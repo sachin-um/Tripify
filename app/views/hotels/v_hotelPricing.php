43507a6f6f213fcc34e2c20a24719fba4a47064d
GimBlznf <88468262+GimBlznf@users.noreply.github.com>
2022-11-23 12:40:19 +0530

    new page to hotel registration

--- app/views/hotels/v_hotelPricing.php
+++ app/views/hotels/v_hotelPricing.php
@@ -7,5 +7,17 @@
 <br>
 
 <div class="pricing-page">
-    
+    Tell us about your first room. 
+    After entering all the necessary info, you can fill in the details of your other rooms.
+    <div class="first-div">
+
+    </div>
+
+    <div class="scnd-div">
+
+    </div>
+
+    <div class="thrd-div">
+
+    </div>
 </div>
--- app/views/hotels/v_hotelReg.php
+++ app/views/hotels/v_hotelReg.php
@@ -4,44 +4,69 @@
 <div class="header-space">
 
 </div>
-<br>
 
 <div class="hotel-page">
+    <div class="main-flex">
+        <div class="first-info">
+            <br>
+            <h4>What is the name of your property?</h4>
+            <input type="text" class="info" id="name" name="name">
 
-    <div class="first-info">
-        <h3>What is the name of your property?</h3>
-        <input type="text" class="info" id="name" name="name">
+            <h4>Star Rating</h4>
+            <input type="text" class="info" id="rating" name="rating">
+        </div>
 
-        <h3>Star Rating</h3>
-        <input type="text" class="info" id="rating" name="rating">
+        <div class="scnd-info">
+            <br>
+            <h4>Contact name</h4>
+            <input type="text" class="info" id="contact" name="contact">
+
+            <h4>Phone number</h4>
+            <input type="text" class="info" id="phone" name="phone">
+        </div>
     </div>
-
-    <div class="scnd-info">
-        <h2>Contact Details</h2>
-
-        <h3>Contact name</h3>
-        <input type="text" class="info" id="contact" name="contact">
-
-        <h3>Phone number</h3>
-        <input type="text" class="info" id="phone" name="phone">
-    </div>
-
+    
+    <br>
+    <br>
     <div class="thrd-info">
+        <br>
         <h2>Property Address</h2>
-
-        <h3>Address Line 1</h3>
-        <input type="text" class="info" id="line-1" name="line-1">
-
-        <h3>Address Line 2</h3>
-        <input type="text" class="info" id="line-2" name="line-2">
-
-        <h3>Address Line 3</h3>
-        <input type="text" class="info" id="line-3" name="line-3">
-
-        <h3>City</h3>
-        <input type="text" class="info" id="city" name="city">
+        <br>
+        <div class="flex-2">
+            <div class="h-address">
+                <h4>Address Line 1</h4>
+                <input type="text" class="info-2" id="line-1" name="line-1">
+            </div>
+            
+            <div class="h-address">
+                <h4>Address Line 2</h4>
+                <input type="text" class="info-2" id="line-2" name="line-2">
+            </div>
+            
+        </div>
+        
+        <div class="flex-2">
+            <div class="h-address">
+                <h4>Address Line 3</h4>
+                <input type="text" class="info-2" id="line-3" name="line-3">
+            </div>
+            
+            <div class="h-address">
+                <h4>City</h4>
+                <input type="text" class="info-2" id="city" name="city">
+            </div>
+            
+        </div>
+        
     </div>
+
+    <br>
+    <div class="btn-info">
+        <button class="btn">Continue</button>
+    </div>
+    
 
 
 </div>
 
+<?php require APPROOT.'/views/inc/components/footer.php'; ?>
