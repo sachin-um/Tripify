b021b3fa61a73a873bba6c7ab41c00a32802634b
lasindu wathsan <lasinduwathsan@gmail.com>
2023-02-10 07:44:07 +0530

    Guide table

--- app/views/guide/v_guide_bookings.php
+++ app/views/guide/v_guide_bookings.php
@@ -36,70 +36,86 @@
                     <th>Location</th>
                     <th>Payment</th>
                     <th>Payment Status</th>
+                    <th>Accept Booking</th>
+                    <th>Ignore Booking</th>
                 </tr>
                 <tr>
-                    <td>John </td>
+                    <td>2023-09-3 </td>
                     <td>Doe </td>
-                    <td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
-                    <td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
+                    <td>2023-11-10 </td>
+                    <td>10 AM </td>
+                    <td>3 Days </td>
+                    <td>Colombo</td>
+                    <td>50USD </td>
+                    <td>card </td>
+                    <td><button id="guide_booking">Accept</button></td>
+                    <td><button id="guide_booking">Ignore</button></td>
 
                 </tr>
                 <tr>
-                    <td>steve </td>
-                    <td>Doe </td>
-                    <td>28 </td>
-                    <td>USA </td>
-                    <td>Male </td>
-                    <td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
+                <td>2023-02-14 </td>
+                    <td>john </td>
+                    <td>2023-09-30 </td>
+                    <td>8 AM </td>
+                    <td>10 Days </td>
+                    <td>panadura</td>
+                    <td>50USD </td>
+                    <td>card </td>
+                    <td><button id="guide_booking">Accept</button></td>
+                    <td><button id="guide_booking">Ignore</button></td>
 
                 </tr>
                 <tr>
-                    <td>simo </td>
-                    <td>Doe </td>
-                    <td>26 </td>
-                    <td>USA </td>
-                    <td>Male </td><td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
+                <td>2023-05-30 </td>
+                    <td>dave </td>
+                    <td>2023-10-10 </td>
+                    <td>11 AM </td>
+                    <td>5 Days </td>
+                    <td>kaluthara</td>
+                    <td>55USD </td>
+                    <td>card </td>
+                    <td><button id="guide_booking">Accept</button></td>
+                    <td><button id="guide_booking">Ignore</button></td>
 
                 </tr>
                 <tr>
-                    <td>karim </td>
-                    <td>Doe </td>
-                    <td>21 </td>
-                    <td>USA </td>
-                    <td>Male </td>
-                    <td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
+                <td>2023-06-15 </td>
+                    <td>shevon </td>
+                    <td>2023-08-10 </td>
+                    <td>6 AM </td>
+                    <td>4 Days </td>
+                    <td>Katunayake</td>
+                    <td>50USD </td>
+                    <td>card </td>
+                    <td><button id="guide_booking">Accept</button></td>
+                    <td><button id="guide_booking">Ignore</button></td>
 
                 </tr>
                 <tr>
-                    <td>adam </td>
+                <td>2023-05-31 </td>
                     <td>Doe </td>
-                    <td>20 </td>
-                    <td>USA </td>
-                    <td>Male </td>
-                    <td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
+                    <td>2024-04-10 </td>
+                    <td>12 PM </td>
+                    <td>7 Days </td>
+                    <td>Colombo</td>
+                    <td>50USD </td>
+                    <td>card </td>
+                    <td><button id="guide_booking">Accept</button></td>
+                    <td><button id="guide_booking">Ignore</button></td>
+
 
                 </tr>
                 <tr>
-                    <td>keven </td>
+                <td>2023-03-3 </td>
                     <td>Doe </td>
-                    <td>20 </td>
-                    <td>USA </td>
-                    <td>Male </td>
-                    <td>25 </td>
-                    <td>USA </td>
-                    <td>Male </td>
+                    <td>2023-01-10 </td>
+                    <td>2 PM </td>
+                    <td>5 Days </td>
+                    <td>Colombo</td>
+                    <td>50USD </td>
+                    <td>card </td>
+                    <td><button id="guide_booking">Accept</button></td>
+                    <td><button id="guide_booking">Ignore</button></td>
 
                 </tr>
                 
