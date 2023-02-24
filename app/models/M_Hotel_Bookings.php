d9cf16fd55edccded636b260afe42c03d19da328
Sachin Umayangana <60028389+sachin-um@users.noreply.github.com>
2023-02-24 18:24:00 +0530

    Update M_Hotel_Bookings.php

--- app/models/M_Hotel_Bookings.php
+++ app/models/M_Hotel_Bookings.php
@@ -10,65 +10,9 @@
 
 
 
-        //add taxi request
+        
 
-        public function addtaxirequest($data){
-            $this->db->query('INSERT INTO taxi_request(Date,StartingTime,Description,TravelerID,PickupLocation,Destination,p_latitude,p_longitude,d_latitude,d_longitude,Caption) 
-            VALUES(:date,:time,:description,:travelerid,:pickuplocation,:destination,:p_latitude,:p_longitude,:d_latitude,:d_longitude,:caption)');
-            $this->db->bind(':caption',$data['caption']);
-            $this->db->bind(':date',$data['date']);
-            $this->db->bind(':time',$data['time']);
-            $this->db->bind(':description',$data['description']);
-            $this->db->bind(':travelerid',$data['travelerid']);
-            $this->db->bind(':pickuplocation',$data['pickuplocation']);
-            $this->db->bind(':destination',$data['destination']);
-            $this->db->bind(':p_latitude',$data['p-latitude']);
-            $this->db->bind(':p_longitude',$data['p-longitude']);
-            $this->db->bind(':d_latitude',$data['d-latitude']);
-            $this->db->bind(':d_longitude',$data['d-longitude']);
-
-            
-
-            if ($this->db->execute()) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        //edit taxi request
-        public function edittaxirequest($data){
-            $this->db->query('UPDATE taxi_request set Date=:date,StartingTime=:time,Description=:description,TravelerID=:travelerid,PickupLocation=:pickuplocation,Destination=:destination,p_latitude=:p_latitude,p_longitude=:p_longitude,d_latitude=:d_latitude,d_longitude=:d_longitude,Caption=:caption WHERE RequestID=:request_id');
-            $this->db->bind(':caption',$data['caption']);
-            $this->db->bind(':date',$data['date']);
-            $this->db->bind(':time',$data['time']);
-            $this->db->bind(':description',$data['description']);
-            $this->db->bind(':travelerid',$data['travelerid']);
-            $this->db->bind(':pickuplocation',$data['pickuplocation']);
-            $this->db->bind(':destination',$data['destination']);
-            $this->db->bind(':p_latitude',$data['p-latitude']);
-            $this->db->bind(':p_longitude',$data['p-longitude']);
-            $this->db->bind(':d_latitude',$data['d-latitude']);
-            $this->db->bind(':d_longitude',$data['d-longitude']);
-            $this->db->bind(':request_id',$data['request_id']);
-
-            
-
-            if ($this->db->execute()) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        public function viewall(){
-            $this->db->query('SELECT * FROM v_taxi_request');
-            $posts=$this->db->resultSet();
-
-            return $posts;
-        }
+        
 
         //Add hotel booking
         public function addHotelBooking(){
